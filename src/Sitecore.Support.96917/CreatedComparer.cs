1d7ba0e89a78fbddc0349a372b486cf661d6aece
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-08-07 14:43:28 -0400

    Init, using code from patch provided in 453560 but putting into official patch

---
+++ src/Sitecore.Support.96917/CreatedComparer.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Data.Comparers;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using System;
+namespace Sitecore.Support.Data.Comparers
+{
+  public class CreatedComparer : Sitecore.Data.Comparers.CreatedComparer
+  {
+    public override IKey ExtractKey(Item item)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      KeyObj keyObj = new KeyObj();
+      keyObj.Item = item;
+      keyObj.Key = GetCreationDate(item);
+      keyObj.Sortorder = item.Appearance.Sortorder;
+      return keyObj;
+    }
+
+    private DateTime GetCreationDate(Item item)
+    {
+      DateTime dateTime = DateTime.MaxValue;
+      Item[] versions = item.Versions.GetVersions(true);
+      Item[] array = versions;
+      foreach (Item item2 in array)
+      {
+        DateTime created = item2.Statistics.Created;
+        if (created != DateTime.MinValue && created.CompareTo(dateTime) < 0)
+        {
+          dateTime = created;
+        }
+      }
+      if (!(dateTime == DateTime.MaxValue))
+      {
+        return dateTime;
+      }
+      return DateTime.MinValue;
+    }
+  }
+
+}
---
+++ src/Sitecore.Support.96917/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.96917")]
+[assembly: AssemblyProduct("Sitecore.Support.96917")]
+[assembly: ComVisible(false)]
